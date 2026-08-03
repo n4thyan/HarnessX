c2e8049b41c5eb9d4c13fc9141ca11dd7679f515
Nathan <101719115+n4thyan@users.noreply.github.com>
2026-08-03 10:58:58 +0100

    Add Studio fuzz client runner

---
+++ roblox/Fuzzer.client.lua
@@ -0,0 +1,8 @@
+if not game:GetService("RunService"):IsStudio() then return nil end
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Fuzzer = require(
+	ReplicatedStorage:WaitForChild("HarnessX"):WaitForChild("Fuzzer")
+)
+
+Fuzzer.Run()
